d9442bc6331bc11db76db4dadd6476a0db790dc0
Cécile Gaudron <98267339+cecilegaudron@users.noreply.github.com>
2023-06-09 09:26:02 +0000

    Remove env.py file

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# compte ce.gaudron database 'PersonalWebsite'
-os.environ["DATABASE_URL"] = "postgres://ptvhalqf:So0NL0eCOv9dqrJ_ARfpVV3gvMaisQOO@mouse.db.elephantsql.com/ptvhalqf"
-os.environ["SECRET_KEY"] = "bbp)z_^$f22l+brkejbm6!kb6a%lfye7a-8o%8ni377syf*3hd"
-# compte ce.gaudron folder 'portfolio'
-os.environ["CLOUDINARY_URL"] = "CLOUDINARY_URL=cloudinary://431114882147935:R_lvwFAMiHtZyCdgg3BeAEj8QcI@di8utbjhc"
