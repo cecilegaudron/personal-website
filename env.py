4c2b4efabdf312650f7da0a83410fe3e2abad85c
Cécile Gaudron <98267339+cecilegaudron@users.noreply.github.com>
2023-06-09 09:24:32 +0000

    Change secret key

--- env.py
+++ env.py
@@ -2,6 +2,6 @@ import os
 
 # compte ce.gaudron database 'PersonalWebsite'
 os.environ["DATABASE_URL"] = "postgres://ptvhalqf:So0NL0eCOv9dqrJ_ARfpVV3gvMaisQOO@mouse.db.elephantsql.com/ptvhalqf"
-os.environ["SECRET_KEY"] = "1+0lt9e7c#@5msq4=o719djl4i^uxue1$m1wkesln^w)8-_(t1"
+os.environ["SECRET_KEY"] = "bbp)z_^$f22l+brkejbm6!kb6a%lfye7a-8o%8ni377syf*3hd"
 # compte ce.gaudron folder 'portfolio'
 os.environ["CLOUDINARY_URL"] = "CLOUDINARY_URL=cloudinary://431114882147935:R_lvwFAMiHtZyCdgg3BeAEj8QcI@di8utbjhc"
